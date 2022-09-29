994025a1553953045bd6511f8756e80c5e50021b
shaquille19 <shakil.m.19@gmailcom>
2022-09-29 14:31:17 +0600

    fixed click target in bonus time

--- src/Components/AddBreak/AddBreak.js
+++ src/Components/AddBreak/AddBreak.js
@@ -5,26 +5,21 @@ const AddBreak = ({ setBonusTimeOnClick }) => {
     <div>
       <h2 className="text-xl font-semibold">Add A Break</h2>
       <div className="bg-teal-50 py-3 px-5 my-5 rounded-xl flex justify-between">
-        <div className="bg-white rounded-lg border-2 border-green-400">
-          <button className="text-lg py-1 px-3 ">
-            <span onClick={(e) => setBonusTimeOnClick(e)}>10</span>s
-          </button>
-        </div>
-        <div className="bg-white rounded-lg border-2 border-green-400">
-          <button className="text-lg py-1 px-3">
-            <span onClick={(e) => setBonusTimeOnClick(e)}>20</span>s
-          </button>
-        </div>
-        <div className="bg-white rounded-lg border-2 border-green-400">
-          <button className="text-lg py-1 px-3">
-            <span onClick={(e) => setBonusTimeOnClick(e)}>30</span>s
-          </button>
-        </div>
-        <div className="bg-white rounded-lg border-2 border-green-400">
-          <button className="text-lg py-1 px-3">
-            <span onClick={(e) => setBonusTimeOnClick(e)}>40</span>s
-          </button>
-        </div>
+        <button className="text-lg py-1 px-3 bg-white rounded-lg border-2 border-green-400">
+          <span onClick={(e) => setBonusTimeOnClick(e)}>10</span>s
+        </button>
+
+        <button className="text-lg py-1 px-3 bg-white rounded-lg border-2 border-green-400">
+          <span onClick={(e) => setBonusTimeOnClick(e)}>20</span>s
+        </button>
+
+        <button className="text-lg py-1 px-3 bg-white rounded-lg border-2 border-green-400">
+          <span onClick={(e) => setBonusTimeOnClick(e)}>30</span>s
+        </button>
+
+        <button className="text-lg py-1 px-3 bg-white rounded-lg border-2 border-green-400">
+          <span onClick={(e) => setBonusTimeOnClick(e)}>40</span>s
+        </button>
       </div>
     </div>
   );
